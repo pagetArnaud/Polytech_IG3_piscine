3b38804c121d20102455b0a8baa471b38141ef9a
Cantero <clement.cantero@etu.umontpellier.fr>
2020-12-23 16:24:05 +0100

    create event

--- router/admin.js
+++ router/admin.js
@@ -1,27 +1,44 @@
 var express = require('express');
 var router = express.Router();
-var controller_admin = require('../controller/admin');
-var auth = require("../lib/auth");
+var model_etudiant = require('../model/etudiant');
+var model_creneau = require('../model/creneau');
+var model_event = require("../model/evenement");
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-router.use(function (req, res, next) {
-    var cookie = req.cookies["session"];
-    var token = auth.getTokenCookie(cookie);
-    if (token) {
-        console.log("On est dans admin et un token");
-        if (token.isAdmin) {//Si on est bien un admin
-            req.token = token; //On passe le token au prochain middleware si il est bien décrypté
-            next()
-        } else {//sinon
-            res.status(403).send("Accès interdit: vous n'êtes pas ADMIN");
-        }
-    } else { //Si on ne peut pas decripter le token ou si le cookie n'existe pas, on demande de se re-login
-        console.log("probleme de décodage du token");
-        res.redirect('../login')
-    }
+router.post(urlencodedParser)
+router.use('/evenement', urlencodedParser)
+
+
+/*
+router.post("/evenement", function(req, res){
+    console.log("oui")
 });
+/*
+router.get('/evenement', function (req, res) {
+    res.send("ça marche");
+});*/
+router.post("/evenement", function (req, res) {
+    var prom = model_event.addEvenement(req.body.name,req.body.dateDebut,req.body.dureeEvent,req.body.dateLimiteResa,req.body.dureeCreneau,req.body.nbrJury,req.body.promo);
+    prom.then((value) => {
+
+        res.send("Evenement créé !");
+
+    }).catch(
+        function (){
+            console.log("Formulaire non rempli correctement")
+            res.send("Formulaire non rempli correctement");
+        }
+    );
+});
+/*
+router.post('/evenement', function (req, res) {
+    console.log(req.body);
+});*/
+
 // Home page route.
 router.get('/', function (req, res) {
-    res.send("bienvenu chez les admin")
+    res.send('admin home page');
 });
 
 // About page route.
@@ -30,18 +47,32 @@ router.get('/groupe', function (req, res) {
 });
 
 router.get('/evenement', function (req, res) {
-    buildFile(req, res, path.join(__dirname, "../vue/evenement/create.ejs"));
-
+    res.sendFile(path.join(__dirname, "../vue/evenement/create.html"));
+    model_creneau.getcreneau(req.body.num);
 });
-
 
 router.get('/creneau', function (req, res) {
-    controller_admin.getAllCreneau(req, res);
+    res.sendFile(path.join(__dirname, "../vue/creneau/selection.html"));
 });
 
-
-router.get('/groupe', function (req, res) {
-    res.send('admin page groupe');
+router.get('/creneau/consultation', function (req, res) {
+    res.sendFile(path.join(__dirname, "../vue/creneau/consultation_creneau.html"));
 });
+
+router.get('/creneau/read', function (req, res) {
+    var prom =model_creneau.getAllcreneau();
+    prom.then((value) => {
+
+        res.send(value);
+
+    }).catch(
+        function (){
+            console.log("y'a une erreur dans la fonction ")
+            res.send("error");
+        }
+    );
+
+});
+
 
 module.exports = router;
--- router/main.js
+++ router/main.js
@@ -1,60 +1,29 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const etu = require(path.join(__dirname, "etu"));
-const admin = require(path.join(__dirname, "admin"));
-const cookieParser = require('cookie-parser');
-const PORT = process.env.PORT || 5000;
-const controller_etu = require('../controller/etudiants');
-const bodyParser = require('body-parser');
-const registration = require('../controller/registration');
-const login = require('../controller/login');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var fs = require('fs');
+var etu=require(path.join(__dirname,"etu"));
+var admin=require(path.join(__dirname,"admin"));
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-
-//TODO faire en sorte qu'on ne puisse pas accéder aux pages de login une fois connecté -> rediriger vers le menu admin ou etu
-
-
-app.set('view engine', 'ejs');//pour utliser le moteur de view EJS
-app.set('views', path.join(__dirname, '/../vue'));
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser());
 
-
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get("/", function (req, res) {
-    var cookie = req.cookies["session"];
-    console.log("on passe pas /");
-    if (cookie) {//existe
-        res.redirect('login')
-    } else {
-    res.render('connexion/index');
-    }
+    res.setHeader('Content-Type', 'text/plain');
+    res.send("hello world");
 
 });
-
 
 //Fonction personnel
 app.get("/log", function (req, res) {
     let date = new Date().toISOString();
     date = date.substring(0, 10) + ".log";
-    res.sendFile("/home/1607nono/admin/logs/sites/2021/sites-" + date);
-});
-//TODO appeler middleware proprement
-//Partie login-------------------------------------------------
-app.get('/login', function (req, res) {
-    controller_etu.login(req, res);
+    res.sendFile("/home/1607nono/admin/logs/sites/2020/sites-" + date);
 });
 
-app.get('/register', function (req, res) {
-    controller_etu.register(req, res)
-
-});
-app.post('/register', registration.checkDataIsValidAndSanitize, registration.checkCorrectness, registration.correctForm);
-//Login
-app.post('/login', login.login);
-
-//----------------------------------
 app.use("/etu",etu);//routeur vers les etudiants
 app.use("/admin",admin);//routeur admin
 
@@ -64,6 +33,7 @@ app.use(function (req, res) {
 
 });
 
-http.listen(PORT, function () {
-    console.log('Listening  ' + PORT)
+
+http.listen(3000, function () {
+    console.log('Listening  ')
 });
