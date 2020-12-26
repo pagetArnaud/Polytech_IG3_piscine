ae9069e475442a6554addddb4939ba6067d3b030
Arnaud Paget <arnaud.paget@etu-umontpellier.fr>
2020-12-26 13:41:30 +0100

    application dynamique de la vue consultationCreneau et son appel par le controller etudiant

--- controller/etudiants.js
+++ controller/etudiants.js
@@ -1,29 +1,23 @@
 var model_etudiant = require('../model/etudiant');
 var model_creneau = require('../model/creneau');
-const promoModel = require('../model/promo')
+var auth = require('../lib/auth');
 
 function login(req, res) {
     res.render("connexion/login", {alreadyRegistered : false, loginFailed : false})
 }
 
 function register(req, res) {
-    promo = promoModel.getAllPromo()
-    promo.then((result) => {
-        res.render("connexion/register", {promo : result})
-    }).catch((err) => {
-        console.log(err)
-        res.render("connexion/register", {promo : []})
-    })
-
+    res.render("connexion/register")
 }
 
 
 function get_creneau(req, res) {
-    var prom = model_creneau.getCreneauEtu(idEtu);
+    var etu = auth.decrypte(req.cookies["session"]);
+    var prom = model_creneau.getCreneauEtu(etu.numEtu);
 
     prom.then((value) => {
-        //console.log(value);
-        res.render("creneau/consultationCreneau", {data: value[0]});
+
+        res.render("creneau/consultationCreneau", {data: value[0], etu: etu});
 
     }).catch((error) => {
 
