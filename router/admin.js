cb5c49efaa04b2ed4002c8bde0f72c7ce37ad57f
Arnaud Paget <arnaud.paget@etu-umontpellier.fr>
2021-01-10 15:25:49 +0100

    merge avec clement

--- router/admin.js
+++ router/admin.js
@@ -5,29 +5,31 @@ var model_creneau = require('../model/creneau');
 var model_event = require("../model/evenement");
 var bodyParser = require("body-parser");
 var controller_admin = require('../controller/admin');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var auth = require("../lib/auth");
 
-router.post(urlencodedParser)
-router.use('/evenement', urlencodedParser)
-
-
+router.use(function (req, res, next) {
+    var cookie = req.cookies["session"];
+    var token = auth.getTokenCookie(cookie);
+    if (token) {
+        console.log("On est dans admin et un token");
+        if (token.isAdmin) {//Si on est bien un admin
+            req.token = token; //On passe le token au prochain middleware si il est bien décrypté
+            next()
+        } else {//sinon
+            res.status(403).send("Accès interdit: vous n'êtes pas ADMIN");
+        }
+    } else { //Si on ne peut pas decripter le token ou si le cookie n'existe pas, on demande de se re-login
+        console.log("probleme de décodage du token");
+        res.redirect('../login')
+    }
+});
 //Envoie formulaire à bdd
 router.post("/evenement", function (req, res) {
     controller_admin.addEvenement(req, res);
 });
-
-
-/*
-router.post("/creneau/modification", function (req, res) {
-    console.log("salut");
-    model_creneau.updateByGroup(req.body.groupe);
-
-});*/
-
-
 // Home page route.
 router.get('/', function (req, res) {
-    controller_admin.login(req, res);
+    res.send("bienvenu chez les admin")
 });
 
 // About page route.
@@ -37,6 +39,7 @@ router.get('/groupe', function (req, res) {
 
 router.get('/evenement', function (req, res) {
     res.render(path.join(__dirname, "../vue/evenement/create"));
+    //TODO Effacer ?
     model_creneau.getcreneau(req.body.num);
 });
 
@@ -51,13 +54,15 @@ router.get('/creneau/modification', function (req, res) {
     controller_admin.modif_creneau(req, res);
 });
 
-router.post('/creneau/modification', function(req,res) {
+router.post('/creneau/modification', function (req, res) {
+    //TODO effacer les deux lignes
     controller_admin.getCreneau(req, res);
     controller_admin.getGroupe(req, res);
     controller_admin.reserveCreneau(req, res);
-
 });
-
+router.get('/groupe', function (req, res) {
+    res.send('admin page groupe');
+});
 
 
 router.get('/creneau/consultation', function (req, res) {
@@ -65,18 +70,20 @@ router.get('/creneau/consultation', function (req, res) {
     controller_admin.consult_creneau(req, res);
 });
 
-router.post('/creneau/consultation', function(req, res) {
+//TODO à effacer
+router.post('/creneau/consultation', function (req, res) {
     controller_admin.getCreneau(req, res);
 })
 
+//TODO à effacer
 router.get('/creneau/read', function (req, res) {
-    var prom =model_creneau.getAllcreneau();
+    var prom = model_creneau.getAllcreneau();
     prom.then((value) => {
 
         res.send(value);
 
     }).catch(
-        function (){
+        function () {
             console.log("y'a une erreur dans la fonction ")
             res.send("error");
         }
