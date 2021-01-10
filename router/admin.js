fed1c182009e32d1b24126537082d1cdd38a098c
Arnaud Paget <arnaud.paget@etu-umontpellier.fr>
2021-01-10 12:42:47 +0100

    merge qui marche pas

--- router/admin.js
+++ router/admin.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var controller_admin = require('../controller/admin');
+var auth = require("../lib/auth");
 
+router.use(function (req, res, next) {
+    var cookie = req.cookies["session"];
+    var prom = auth.getTokenCookie(cookie);
+    prom.then((token) => {
+        if (token.isAdmin) {//Si on est bien un admin
+            req.token = token; //On passe le token au prochain middleware si il est bien décrypté
+            next()
+        } else {
+            res.status(403).send("Accès interdit: vous n'êtes pas ADMIN");
+        }//sinon
+    }).catch((msg) => { //Si on ne peut pas decripter le token ou si le cookie n'existe pas, on demande de se re-login
+        console.log(msg);
+        res.redirect('../login')
+    })
+});
 // Home page route.
 router.get('/', function (req, res) {
     controller_admin.login(req, res);
