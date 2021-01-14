8add2893671281141b9b40ae1e64704d9353d822
quentin B <oliquent@orange.fr>
2021-01-14 11:38:29 +0100

    pas fait le css pour être raccord avec le reste

---
+++ controller/groupe.js
@@ -0,0 +1,157 @@
+var model_groupe = require('../model/groupe');
+
+function getGroupe(req, res) {
+    var prom = model_groupe.getGroupeEleve(req.token.numEtu);
+    
+    prom.then((value) => {
+        console.log(value)
+        res.render('groupe/consultation_de_groupe', {groupe:value})
+
+    }).catch((error) => {
+
+            console.log(error);
+            res.send(error);
+        }
+    );
+    
+}
+
+function addGroupe(req, res) {
+    var prom = model_groupe.addGroupe(req.body.NomTuteur,req.body.PrenomTuteur,req.body.EntrepriseTut,req.body.Prof,0,req.body.nomGrp)
+    prom.then((value) => {
+        
+        var idB = req.body.numEleveB;
+        var idC = req.body.numEleveC;
+        var idD = req.body.numEleveD;
+        var idE = req.body.numEleveE;
+    
+        if (0==model_groupe.checkEtu(req.body.numEleveA)) {//check si l'élève existe
+            var prom1 = model_groupe.DeleteGrpLast()
+        }else {var prom1 = model_groupe.addComposer(req.body.numEleveA)}
+    
+        if ("None"!=idB) {//check pour savoir combien y a d'élèves à rajouter (max 5)        
+            if (0==model_groupe.checkEtu(idB)) {//check si l'élève existe
+                var prom2 = 1;
+            }else {var prom2 = model_groupe.addComposer(idB)}
+        }
+        if ("None"!=idC) {//check pour savoir combien y a d'élèves à rajouter (max 5)        
+            if (0==model_groupe.checkEtu(idC)) {//check si l'élève existe
+                var prom3 = 1;
+            }else {var prom3 = model_groupe.addComposer(idC)}
+        }
+        if ("None"!=idD) {//check pour savoir combien y a d'élèves à rajouter (max 5)        
+            if (0==model_groupe.checkEtu(idD)) {//check si l'élève existe
+                var prom4 = 1;
+            }else {var prom4 = model_groupe.addComposer(idD)}
+        }
+        if ("None"!=idE) {//check pour savoir combien y a d'élèves à rajouter (max 5)        
+            if (0==model_groupe.checkEtu(idE)) {//check si l'élève existe
+                var prom5 = 1;
+            }else {var prom5 = model_groupe.addComposer(idE)}
+        }
+        Promise.all.([prom1,prom2,prom3,prom4,prom5]).then((value) => {
+            res.redirect('/etu/groupe/')
+        }).catch((error) => {
+    
+            console.log(error);
+            res.send(error);
+            res.redirect('/etu/groupe/')
+            }
+        );
+
+    
+
+        }).catch((error) => {
+
+            console.log(error);
+            res.send(error);
+            res.redirect('/etu/groupe/')
+        }
+    );
+    
+}
+
+function ModGroupe(req, res) { //on regarde où on doit faire une modification
+    var prom = 1 ;
+    var prom1 = 1 ;
+    var prom2 = 1 ;
+    var prom3 = 1 ;
+    var prom4 = 1 ;
+    var prom5 = 1 ;
+    var prom6 = 1 ;
+    var prom7 = 1 ;
+    var prom8 = 1 ;
+    var prom10 = 1 ;
+    var prom9 = 1 ;
+    
+    if (req.body.NomGroupe != "None") {
+        var prom = model_groupe.modNomGroupe(req.body.idGroupe, req.body.NomGroupe)
+    }
+    if (req.body.NomTuteur != "None") {
+        var prom1 = model_groupe.modNomTuteur(req.body.idGroupe, req.body.NomTuteur)
+    }
+    if (req.body.PrenomTuteur != "None") {
+        var prom2 = model_groupe.modPrenomTuteur(req.body.idGroupe, req.body.PrenomTuteur)
+    }
+    if (req.body.EntrepriseTut != "None") {
+        var prom3 = model_groupe.modEntrepriseTut(req.body.idGroupe, req.body.EntrepriseTut)
+    }
+    if (req.body.Prof != "None") {
+        var prom4 = model_groupe.modProf(req.body.idGroupe, req.body.Prof)
+    }
+    if (req.body.numEleveA != "None" || req.body.numEleveB != "None" || req.body.numEleveC != "None" || req.body.numEleveD != "None" || req.body.numEleveE != "None" ) {
+        var prom5 = model_groupe.DeleteGrpc(req.body.idGroupe)
+        if (req.body.numEleveA != "None") {
+            var prom6 = model_groupe.modnumEleve(req.body.idGroupe, req.body.numEleveA)
+        }
+        if (req.body.numEleveB != "None") {
+            var prom7 = model_groupe.modnumEleve(req.body.idGroupe, req.body.numEleveB)
+        }
+        if (req.body.numEleveC != "None") {
+            var prom8 = model_groupe.modnumEleve(req.body.idGroupe, req.body.numEleveC)
+        }
+        if (req.body.numEleveD != "None") {
+            var prom9 = model_groupe.modnumEleve(req.body.idGroupe, req.body.numEleveD)
+        }
+        if (req.body.numEleveE != "None") {
+            var prom10 = model_groupe.modnumEleve(req.body.idGroupe, req.body.numEleveE)
+        }
+    }
+    Promise.all.apply([prom,prom1,prom2,prom3,prom4,prom5,prom6,prom7,prom8,prom10,prom9]).then((value) => {
+        res.redirect('/etu/groupe/')
+
+    }).catch((error) => {
+
+            console.log(error);
+            res.send(error);
+            res.redirect('/etu/groupe/')
+        }
+    );
+    
+}
+
+function DeleteGroupe(req, res) {
+    var prom = model_groupe.DeleteGrpc(req.body.idGroupe)
+    prom.then((value) => {
+        var prom1 = model_groupe.DeleteGrp(req.body.idGrp)
+        prom1.then((value) => {
+            res.redirect('/etu/groupe/')
+        
+        }).catch((error) => {
+
+            console.log(error);
+            res.send(error);
+            res.redirect('/etu/groupe/')
+        }
+    }).catch((error) => {
+
+            console.log(error);
+            res.send(error);
+            res.redirect('/etu/groupe/')
+        }
+    );
+    
+    
+    
+}
+module.exports = {DeleteGroupe,getGroupe,addGroupe,ModGroupe};
--- model/groupe.js
+++ model/groupe.js
@@ -1,3 +1,210 @@
 path = require("path")
 var bd = require(path.join(__dirname, "../lib/conf"));
 
+
+//fonction pour créer un groupe
+function addGroupe(nomTuteurEntreprise,prenomTuteurEntreprise,nomEntreprise,TuteurEnseignant,nomGrp) {
+    return new Promise((resolve, reject) => {
+        bd.query ('INSERT INTO groupe (nomTuteurEntreprise,prenomTuteurEntreprise,nomEntreprise,TuteurEnseignant,nomGrp) VALUES (?,?,?,?,?,?)',
+        [nomTuteurEntreprise,prenomTuteurEntreprise,nomEntreprise,TuteurEnseignant,nomGrp],
+            function(err, result){
+                if (err){
+                    
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        
+    });
+}
+
+function addComposer(nomEtu) {
+    return new Promise((resolve, reject) => {
+        bd.query ('INSERT INTO composer (etudiant,groupe) VALUES (?,(SELECT MAX(id) FROM groupe))',
+        [nomEtu],
+            function(err, result){
+                if (err){
+                    
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        
+    });
+
+}
+
+//donne les groupes de l'élève
+function getGroupeEleve(IdEleve) {
+    return new Promise((resolve, reject) => {
+
+    bd.query ('SELECT * FROM groupe WHERE id IN (SELECT groupe FROM composer WHERE etudiant=?) ',
+        [IdEleve],
+        function(err, result){
+            if (err){
+                reject(err);
+            }
+            resolve(result);
+        }
+    );
+    });
+}
+
+
+
+function checkEtu(num) { //fonction qui vérifie qu'un numéro pour créer un groupe est bien celui d'un étudiant
+    return new Promise((resolve, reject) => {
+
+        bd.query ('SELECT COUNT(0) FROM etudiant WHERE num=? ',
+            [num],
+            function(err, result){
+                if (err){
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        });
+}
+
+function modNomGroupe(numGrp, nom) {
+    return new Promise((resolve, reject) => {
+
+        bd.query ('UPDATE groupe SET nomGrp = ? WHERE id=?',
+            [nom, numGrp],
+            function(err, result){
+                if (err){
+                    
+                    reject(err);
+                }
+                
+                resolve(result);
+            }
+        );
+        });
+}
+
+
+function modNomTuteur(numGrp, nom) {
+    return new Promise((resolve, reject) => {
+
+        bd.query ('UPDATE groupe SET NomTuteurEntreprise = ? WHERE id=?',
+            [nom,numGrp],
+            function(err, result){
+                if (err){
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        });
+}
+
+function modPrenomTuteur(numGrp, nom) {
+    return new Promise((resolve, reject) => {
+
+        bd.query ('UPDATE groupe SET prenomTuteurEntreprise = ? WHERE id=?',
+            [nom,numGrp],
+            function(err, result){
+                if (err){
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        });
+}
+
+function modEntrepriseTut(numGrp, nom) {
+    return new Promise((resolve, reject) => {
+
+        bd.query ('UPDATE groupe SET nomEntreprise = ? WHERE id=?',
+            [nom,numGrp],
+            function(err, result){
+                if (err){
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        });
+}
+
+function modProf(numGrp, num) {
+    return new Promise((resolve, reject) => {
+
+        bd.query ('UPDATE groupe SET TuteurEnseignant = ? WHERE id=? ',
+            [num,numGrp],
+            function(err, result){
+                if (err){
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        });
+}
+
+function modnumEleve(numGrp, num) {
+    return new Promise((resolve, reject) => {
+
+        bd.query ('INSERT INTO composer (etudiant,groupe) VALUES (?,?)',
+            [num,numGrp],
+            function(err, result){
+                if (err){
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        });
+}
+
+function DeleteGrpc(numGrp) {
+    return new Promise((resolve, reject) => {
+        parseInt(numGrp)
+        bd.query ('DELETE FROM composer WHERE groupe = ?',
+            [numGrp],
+            function(err, result){
+                if (err){
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        });
+}
+
+function DeleteGrp(numGrp) {
+    return new Promise((resolve, reject) => {
+        parseInt(numGrp)
+        bd.query ('DELETE FROM groupe WHERE id = ?',
+            [numGrp],
+            function(err, result){
+                if (err){
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        });
+}
+
+function DeleteGrpLast() {
+    return new Promise((resolve, reject) => {
+        
+        bd.query ('DELETE FROM groupe WHERE id = (SELECT MAX(id) FROM groupe)',
+            
+            function(err, result){
+                if (err){
+                    reject(err);
+                }
+                resolve(result);
+            }
+        );
+        });
+}
+
+module.exports = {DeleteGrpLast,DeleteGrp,addGroupe,getGroupeEleve,checkEtu,DeleteGrpc,modnumEleve,modProf,modEntrepriseTut,modPrenomTuteur,modNomTuteur,modNomGroupe,addComposer};
